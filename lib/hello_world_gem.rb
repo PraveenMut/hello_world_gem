76e019fd8941348e312de51d70075f18300456c4
praveen <praveen.mut@gmail.com>
2019-08-03 14:33:08 +1000

    Initial Commit

---
+++ lib/hello_world_gem.rb
@@ -0,0 +1,6 @@
+require "hello_world_gem/version"
+
+module HelloWorldGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/hello_world_gem_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe HelloWorldGem do
+  it "has a version number" do
+    expect(HelloWorldGem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
