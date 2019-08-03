c81739aa757e11b78ae66badf802ed204ba6d270
praveen <praveen.mut@gmail.com>
2019-08-03 14:46:48 +1000

    oops, change to world

--- lib/hello_world_gem.rb
+++ lib/hello_world_gem.rb
@@ -2,7 +2,7 @@ require "hello_world_gem/version"
 
 module HelloWorldGem
   class Error < StandardError; end
-  def self.hello_word
+  def self.hello_world
     p "Hello World, it's Praveen!"
   end
 end
